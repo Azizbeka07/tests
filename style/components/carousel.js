570e75bc1c467cdf7ea5e7249e55d8971382a1e0
AKTIV <Azizbek>
2023-02-25 22:01:41 +0500

    hello

---
+++ style/components/carousel.js
@@ -0,0 +1,28 @@
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        clickable: true,
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
+});
+
+swiperEl.initialize();
